caaf7a360dc4494161d05cc4fddd6633c7fee5f4
Miska Savenius <miska.savenius@gmail.com>
2025-01-28 12:00:12 +0200

    Muokattu reporting_app.py. Alustetaan Flask

---
+++ src/data/reporting_app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from flask_cors import CORS
+from routes.reporting_api import reporting_api
+
+app = Flask(__name__)
+CORS(app)
+
+# Register blueprints
+app.register_blueprint(reporting_api, url_prefix="/reporting")
+
+if __name__ == "__main__":
+    app.run(debug=True)
