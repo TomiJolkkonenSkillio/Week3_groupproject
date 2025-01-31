f5a3fb50290b218c6df710196f97a85ebecd92db
Miska Savenius <miska.savenius@gmail.com>
2025-01-31 14:08:07 +0200

    Lataaminen luotu

--- src/data/routes/reporting_api.py
+++ src/data/routes/reporting_api.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request, current_app
-from services.reporting_service import get_weekly_working_hours, upload_to_blob, manipulate_data
+from services.reporting_service import get_weekly_working_hours, upload_to_blob, manipulate_data, download_file
 import pandas as pd
 from apscheduler.schedulers.background import BackgroundScheduler
 
@@ -8,12 +8,13 @@ from apscheduler.schedulers.background import BackgroundScheduler
 reporting_api = Blueprint('reporting_api', __name__)
 
 # Create a function for generating a weekly report manually
-@reporting_api.route('/', methods=['POST'])
+@reporting_api.route('/', methods=['GET'])
 def generate_report():
     try:
         df = pd.DataFrame(get_weekly_working_hours())
         df = manipulate_data(df)
         upload_to_blob(df)
-        return jsonify({'message': 'Weekly report generated successfully'}), 201
+        return download_file(df)
+        # return jsonify({'message': 'Weekly report generated successfully'}), 201
     except Exception as e:
         return jsonify({'error': str(e)}), 500
